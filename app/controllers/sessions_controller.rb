2e3fa0bd1c85081eb8aa8447a24e5b9cbc196631
Benoit de Chateauvieux <benoit@HPVN-0045L.local>
2016-06-11 16:17:25 +0700

    implement login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	submitted_user = params[:user]
+  	user = User.find_by(username: submitted_user[:username])
+  	if user && user.authenticate(submitted_user[:password])
+  		session[:user_id] = user.id
+  		redirect_to :root, notice: 'Logged in successfully'
+  	else
+  		redirect_to :login, alert: 'Invalid Login'
+  	end
   end
 
   def destroy
